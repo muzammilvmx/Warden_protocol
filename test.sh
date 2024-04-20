5c379164f213078d7ac8df4f8e0991849500604a
Cipher_Airdrop <138387616+muzammilvmx@users.noreply.github.com>
2024-04-20 14:54:37 +0430

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,6 +12,10 @@ tmux new-session -d -s warden_session
 # Send the command to the tmux session
 tmux send-keys -t warden_session "if ! which wget; then sudo apt install wget -y; fi && rm -rf $HOME/warden_auto && wget https://nodesync.top/warden_auto && chmod +x warden_auto && ./warden_auto" Enter
 
+# Wait for the prompt to appear and then send "Y"
+sleep 2
+tmux send-keys -t warden_session "Y" Enter
+
 # Detach from the tmux session
 tmux detach -s warden_session
 
